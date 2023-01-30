24e440d46d0db7fcc81bfdfb7f9df7d2c97dd815
jedrek89 <marcinj3@gmail.com>
2023-01-30 11:13:44 +0100

    BartCom ver 2

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("js works!");
+
+document.getElementById("year").innerHTML = new Date().getFullYear() + " ";
+
+// document.getElementById('aboutMe').addEventListener('click', function () {
+//     const element = document.getElementById('content1');
+//     element.scroll({behavior: "smooth", block: "start"});
+//     console.log(element);
+    
+// })
+
+// document.getElementById('services').addEventListener('click', function () {
+//     const element = document.getElementById('content2');
+//     element.scroll({behavior: "smooth", block: "start"});
+//     console.log(element);
+// })
+
+// document.getElementById('contact').addEventListener('click', function () {
+//     const element = document.getElementById('content3');
+//     element.scroll({behavior: "smooth", block: "start"});
+//     console.log(element);
+// })
+
+
+// document.getElementById(chr).scrollIntoView(true);
