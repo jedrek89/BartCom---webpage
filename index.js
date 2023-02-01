aee1096bb00d1059b0beea36c47deab92fb2b965
jedrek89 <marcinj3@gmail.com>
2023-02-01 11:20:28 +0100

    New styles and animations on firts page

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 console.log("js works!");
+let showAboutMe;
 
-document.getElementById("year").innerHTML = new Date().getFullYear() + " ";
+// document.getElementById("year").innerHTML = new Date().getFullYear() + " ";
+
+document.getElementById('aboutMeButton').addEventListener('click', function() {
+    let element = document.getElementById('aboutMeDescription');
+    if (element.classList[0] === 'hideItem') {
+        document.getElementById('aboutMeDescription').classList.remove(`${element.classList[0]}`);
+        document.getElementById('aboutMeDescription').classList.add('aboutMeDescription');
+    }
+    else{
+        document.getElementById('aboutMeDescription').classList.remove(`${element.classList[0]}`);
+        document.getElementById('aboutMeDescription').classList.add('hideItem');
+    }
+})
